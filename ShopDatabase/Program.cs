6361b02a7d46d6fcb718d96149e280271bbcad4b
dev <baran_kv97@student.itstep.org>
2024-08-24 12:38:34 +0300

    New text

--- ShopDatabase/Program.cs
+++ ShopDatabase/Program.cs
@@ -10,6 +10,8 @@ public class City
     public string Name { get; set; }
     public int CountryId { get; set; }
     public Country Country { get; set; }
+    public ICollection<Shop> Shops { get; set; }
+
 }
 public class Shop
 {
@@ -18,12 +20,16 @@ public class Shop
     public string Address { get; set; }
     public int CityId { get; set; }
     public int? ParkingArea { get; set; }
+    public City City { get; set; }
+    public ICollection <Worker> Workers { get; set; }
+    public ICollection <Product> Products { get; set; }
 }
 public class Country
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    
+    public ICollection<City> Cities { get; set; }
+
 }
 
 public class Worker
@@ -35,7 +41,9 @@ public class Worker
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
     public int CityId { get; set; }
+    public Position Position { get; set; }
     public int PositionId { get; set; }
+    public Shop Shop { get; set; }
     public int ShopId { get; set; }
     
 }
@@ -44,12 +52,14 @@ public class Category
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public ICollection<Product> Products { get; set; }
 
 }
 public class Position
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public  ICollection<Worker> Workers { get; set; }
 
 }
 
@@ -59,10 +69,16 @@ public class Product
     public string Name { get; set; }
     public double Price { get; set; }
     public float Discount { get; set; }
-    public int? CategoryId { get; set; }
+    public ICollection<Shop> Shops { get; set; }
+
 
     public int Quantity { get; set; }
     public bool IsINStock { get; set; }
+
+    public Category Category { get; set; }
+    public int? CategoryId { get; set; }
+
+
 }
 
 
